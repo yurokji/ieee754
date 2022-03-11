9d7e2b543f2e33efc6239bd4f56b5aa4ae7025e7
Yurok Ji <49470364+yurokji@users.noreply.github.com>
2022-03-11 14:42:41 +0900

    Add files via upload
    bug fix and subtraction added

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from ieee754 import *
 # 범위 <---> 밀도 반비례 관계
 # 32 bit, 64bit, 128bit (밀도)
 # 실수: 0.1 != 000000.00010...(2)
-n1 = "1694.5682373"
+n1 = "2346.8934"
 # 실제 저장되는 수: 1694.5682373046875
 # 0x44d3d22f
-n2 = "438.9865"
+n2 = "8334.35425"
 # 실제 저장되는 수: 438.98651123046875
 # 0x43db7e46
 n3 = n1 + n2
@@ -23,14 +23,17 @@ n3 = n1 + n2
 
 f1 = IEEE754_Float32(n1)
 print("부동소수표현(16진수):", f1.toHex())
+f1.printFixedBinNum(f1.__str__())
 print("실제 저장된 값(10진수):", f1)
 print()
 
 f2 = IEEE754_Float32(n2)
 print("부동소수표현(16진수):", f2.toHex())
+f2.printFixedBinNum(f2.__str__())
 print("실제 저장된 값(10진수):", f2)
 print()
 
 f3 = f1 + f2
 print("부동소수표현(16진수):", f3.toHex())
+f3.printFixedBinNum(f3.__str__())
 print("실제 저장된 값(10진수):", f3)
